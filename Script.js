050d0f61b1f2368142b4dbbd22fe3eb68b753aa9
talhanonari <asimriaz1998@gmail.com>
2024-06-03 14:51:51 +0500

    JavaScript

---
+++ Script.js
@@ -0,0 +1,15 @@
+var x=5;
+var y=6;
+var z;
+z=x+y;
+console.log(z);
+//==================================================================
+function add(a, b) {
+    return a + b;
+}
+
+let n1 = 5;
+let n2 = 10;
+let sum = add(n1, n2);
+console.log("Sum of given numbers is :", sum);
+//===================================================================
