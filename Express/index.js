9a5307c15fa2a3e53ba54db171f49710d43e2676
talhanonari <asimriaz1998@gmail.com>
2024-06-06 13:12:32 +0500

    create Express Server

--- Express/index.js
+++ Express/index.js
@@ -1,8 +1,16 @@
-const http = require('http');
-const data = require('./Data.js')
+const express = require('express');
+const app = express();
 
-http.createServer((req,res)=>{
-    res.writeHead(200, {'Content-Type':'application\json'});
-    res.write(JSON.stringify(data));
-    res.end();
-}).listen(3001);
+app.get('',(req, res)=>{
+    console.log('Request Query=>'+req.query.name)
+    res.send('WelCome To HomePage!!!')
+});
+app.get('/about',(req, res)=>{
+    res.send("WelCome To About Us Page")
+});
+app.get('/*',(req, res)=>{
+    res.send('Error 404: Page Not Found')
+})
+
+
+app.listen(4000);
