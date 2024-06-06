e868f143dbbf2a8bdc91e5b4bf09543c3c0eaded
talhanonari <asimriaz1998@gmail.com>
2024-06-06 15:21:36 +0500

    Get data from MongoDB

--- Express/index.js
+++ Express/index.js
@@ -1,19 +1,26 @@
-const express = require('express');
-const app = express();
+const dbConnect = require('./mongoDb')
 
-app.get('',(req, res)=>{
-    res.send('WelCome To HomePage!!!')
-});
-app.get('/about',(req, res)=>{
-    res.send("WelCome To About Us Page")
-});
-app.get('/contact',(req, res)=>{
-    res.send("WelCome To Contact Us Page")
-});
-app.get('*',(req, res)=>{
-    res.send('Error 404: Page Not Found')
-})
+const dbData = async ()=>{
+    let data = await dbConnect();
+    data = await data.find().toArray();
+    console.log(data)
+}
 
-app.listen(4000,()=>{
-    console.log('Server is Running on Port 4000')
-});
+dbData();
+
+// app.get('',(req, res)=>{
+//     res.send('WelCome To HomePage!!!')
+// });
+// app.get('/about',(req, res)=>{
+//     res.send("WelCome To About Us Page")
+// });
+// app.get('/contact',(req, res)=>{
+//     res.send("WelCome To Contact Us Page")
+// });
+// app.get('*',(req, res)=>{
+//     res.send('Error 404: Page Not Found')
+// })
+
+// app.listen(4000,()=>{
+//     console.log('Server is Running on Port 4000')
+// });
