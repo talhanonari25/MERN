15ef07a71582d6a9f589da8f9d38c79630e5cdeb
talhanonari <asimriaz1998@gmail.com>
2024-06-06 14:42:55 +0500

    Get Api in Express

--- Express/index.js
+++ Express/index.js
@@ -2,15 +2,18 @@ const express = require('express');
 const app = express();
 
 app.get('',(req, res)=>{
-    console.log('Request Query=>'+req.query.name)
     res.send('WelCome To HomePage!!!')
 });
 app.get('/about',(req, res)=>{
     res.send("WelCome To About Us Page")
 });
-app.get('/*',(req, res)=>{
+app.get('/contact',(req, res)=>{
+    res.send("WelCome To Contact Us Page")
+});
+app.get('*',(req, res)=>{
     res.send('Error 404: Page Not Found')
 })
 
-
-app.listen(4000);
+app.listen(4000,()=>{
+    console.log('Server is Running on Port 4000')
+});
